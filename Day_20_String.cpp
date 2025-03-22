1af813c35b1b8495e69b5aac3637ae010013347b
VishalNWL <nrsvishaln@gmail.com>
2025-03-23 01:42:24 +0530

    Update Day_20_String.cpp

--- Day_20_String.cpp
+++ Day_20_String.cpp
@@ -34,3 +34,38 @@ public:
 
 Approach:using hash map to store the frequency and the using max heap so that maxfreq element occur at the top so we can print them
 Time and Space complexity: O(N)
+
+// String to Intiger
+class Solution {
+public:
+    int myAtoi(string s) {
+        bool isnegative=false;
+        long long int num=0;
+        int i=0;
+
+        while(i<s.length()&&s[i]==' '){
+            i++;
+        }
+
+        if(i<s.length() && (s[i]=='-'|| s[i]=='+')){
+            isnegative=(s[i]=='-');
+            i++;
+        }
+        
+        while(i<s.length() && isdigit(s[i])){
+            if (num > (INT_MAX - (s[i]-'0')) / 10) { 
+                return isnegative ? INT_MIN : INT_MAX;
+            }
+            num=num*10+(s[i]-'0');
+            i++;
+        }
+
+
+
+       return isnegative?-1*num: num;
+    }
+};
+
+Appraoch:Handle the edge cases carefully
+Time compleixty: O(N)
+Space compleixty:O(1)
